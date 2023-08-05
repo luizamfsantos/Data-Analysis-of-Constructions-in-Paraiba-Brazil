aaa6e27d4bedddbc7fe933372a71e81aae2be939
Luiza Santos <luiza.marques_@hotmail.com>
2023-08-05 15:31:54 -0300

    Update construcoes vias paraiba analysis.R

--- construcoes vias paraiba analysis.R
+++ construcoes vias paraiba analysis.R
@@ -37,20 +37,45 @@ construcoes_pb$`Valor Medido` <- gsub("\\.","",construcoes_pb$`Valor Medido`) #i
 construcoes_pb$`Valor Medido` <- gsub("\\,","\\.",construcoes_pb$`Valor Medido`)
 construcoes_pb$`Valor Medido` <- as.numeric(construcoes_pb$`Valor Medido`)
 
+
 View(construcoes_pb)
 
 ############################## Data Exploration ###################################
 # Tipos de estagios: 
 unique(construcoes_pb$Estagio) # "Concluída"    "Em execução"  "Em projeto"   "Em licitação" "A iniciar"  
 
+# Time Frame Overall
+min(construcoes_pb$Inicio, na.rm = TRUE) # 2018-03-01
+max(construcoes_pb$Conclusao,na.rm = TRUE) # 2024-09-01 Probably that's expected date
+
+
 # Focar nos que ainda vao aumentar o preco em execucao
 em_execucao <- construcoes_pb[construcoes_pb$Estagio=='Em execução',]
 em_execucao <- em_execucao[,-c(3,5,9,10)]
 View(em_execucao)
+
+# Time Frame of em execucao
+min(em_execucao$Inicio, na.rm = TRUE) # 2020-06-01
+max(em_execucao$Conclusao,na.rm = TRUE) # 2024-09-01 Probably that's expected date
+
 
 # Visualizar os que ja foram concluidas
 concluidas <- construcoes_pb[construcoes_pb$Estagio=='Concluída',]
 concluidas <- concluidas[,-c(3,5,9,10)]
 View(concluidas)
 
+# Time Frame of concluidas
+min(concluidas$Inicio, na.rm = TRUE) # 2018-03-01
+max(concluidas$Conclusao,na.rm = TRUE) # 2023-01-01
+
+theme_classic()
+concluidas %>% 
+  mutate(.,Valor = Valor/1000000) %>%
+  ggplot(aes(x = `Extensao(km)`, y = Valor)) +
+  geom_point() + 
+  labs(subtitle="", 
+       y="Valor em Milhões", 
+       x="Extensão em Kilometros", 
+       title="Construções na Paraiba Durante o Period 2018 a 2023", 
+       caption="Source: midwest")
 
