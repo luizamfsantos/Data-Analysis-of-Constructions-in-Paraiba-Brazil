59ce17aeb265df418948b5c4764cbf074537e269
Luiza Santos <luiza.marques_@hotmail.com>
2023-08-05 11:04:02 -0300

    Update construcoes vias paraiba analysis.R

--- construcoes vias paraiba analysis.R
+++ construcoes vias paraiba analysis.R
@@ -1,10 +1,11 @@
 # import libraries
 library(readr)
 library(lubridate)
+library(dplyr)
 
 construcoes_pb <- read_csv("~/Documents/Projects/Construções Paraiba/Data-Analysis-of-Constructions-in-Paraiba-Brazil/Data/construcoes_pb.csv")
 
-##### Data Cleaning 
+############################## Data Cleaning ###################################
 ### remove id column
 construcoes_pb <- construcoes_pb[,-1]
 
@@ -21,8 +22,22 @@ construcoes_pb$Conclusao <- dmy(paste("1 ",construcoes_pb$Conclusao))
 ### I noticed Extensao was supposed to be in km but was in dm 
 construcoes_pb$`Extensao(km)` <- construcoes_pb$`Extensao(km)`/100
 
+### Switch valor and valor medido to float
+# rewrite to correct format
+construcoes_pb$Valor <- gsub("R\\$ ","",construcoes_pb$Valor,ignore.case = TRUE)
+construcoes_pb$Valor <- gsub("\\.","",construcoes_pb$Valor) #in Brazil , means . and vice versa
+construcoes_pb$Valor <- gsub("\\,","\\.",construcoes_pb$Valor)
+# switch to num type
+options(digits=12)
+construcoes_pb$Valor <- as.numeric(construcoes_pb$Valor)
+# Repeat steps to Valor Medido
+construcoes_pb$`Valor Medido` <- gsub("R\\$ ","",construcoes_pb$`Valor Medido`,ignore.case = TRUE)
+construcoes_pb$`Valor Medido` <- gsub("\\.","",construcoes_pb$`Valor Medido`) #in Brazil , means . and vice versa
+construcoes_pb$`Valor Medido` <- gsub("\\,","\\.",construcoes_pb$`Valor Medido`)
+construcoes_pb$`Valor Medido` <- as.numeric(construcoes_pb$`Valor Medido`)
 
-### Data Exploration
+
+############################## Data Exploration ###################################
 # Tipos de estagios: 
 unique(construcoes_pb$Estagio) # "Concluída"    "Em execução"  "Em projeto"   "Em licitação" "A iniciar"  
 
@@ -30,8 +45,7 @@ unique(construcoes_pb$Estagio) # "Concluída"    "Em execução"  "Em projeto"  
 em_execucao <- construcoes_pb[construcoes_pb$Estagio=='Em execução',]
 em_execucao <- em_execucao[,-c(3,5,9,10)]
 
-
+View(em_execucao)
 
 
 str(em_execucao)
-
